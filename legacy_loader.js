d73445c8f8cbbf8872e024ad5a44fa97411d0e4f
SecZeror <39761156+DevGreick@users.noreply.github.com>
2025-09-08 23:55:24 -0300

    Update legacy_loader.js

--- legacy_loader.js
+++ legacy_loader.js
@@ -1,10 +1,7 @@
 
-# Carregador de Recursos Legados
-# ===================================================================
-
 function getResource() {
  
-  const resourceUrl = "aHR0cHM6Ly9jb250ZW50LmV4YW1wbGUuY29tL2ludm9pY2VfZmRwuIguZXhl";
+  const resourceUrl = "aHR0cHM6Ly9jb250ZW50LmV4YW1wbGUuY29tL2ludm9pY2VfZmRw4oCFLmV4ZQ==";
   
-
+ 
 }
