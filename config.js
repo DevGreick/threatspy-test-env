0161d8c553ddd1e2dad928112bb28843ad064d96
SecZeror <39761156+DevGreick@users.noreply.github.com>
2025-09-08 22:56:30 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// ===================================================================
+# Configurações de Endpoints da Aplicação
+# ===================================================================
+
+const settings = {
+ 
+  apiEndpoint: "aHR0cHM6Ly93d3cu0LDEgXBsZS5jb20vbG9naW4="
+};
+
+module.exports = settings;
